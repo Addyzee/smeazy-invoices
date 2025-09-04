922a6c85e34c9ac30ce29853035dffe357d5e2c3
Addyzee <andrewkamaukim@gmail.com>
2025-09-04 13:22:25 +0300

    change compose

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,15 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://smeazy_user:smeazydon@localhost/smeazyinvoices"
+
+
+DATABASE_URL = (
+    f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}"
+    f"@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}"
+)
+
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

a23b1ca090785c206d88592ad54c964c069ad6d9
Addyzee <andrewkamaukim@gmail.com>
2025-12-02 10:05:45 +0300

    update africa's talking for public

--- backend/app/atalking.py
+++ backend/app/atalking.py
@@ -3,23 +3,28 @@ import africastalking
 
 
 # Initialize SDK
-username = "smeazy_invoices"    # use 'sandbox' for development in the test environment
-api_key = "atsk_28e13f1f258c83c1adad736397c902f3679dd368a094dc97c2b1e83d7addad2280edd39d"      # use your sandbox app API key for development in the test environment
+username = "disabled"    # use 'sandbox' for development in the test environment
+api_key = "your_api_key"      # use your sandbox app API key for development in the test environment
 africastalking.initialize(username, api_key)
 
 
 # Initialize a service e.g. SMS
 sms = africastalking.SMS
 
+if sms is None:
+    raise Exception("AfricasTalking SMS service not initialized properly")
+
+phone_numbers = None
+
+if phone_numbers is None:
+    raise Exception("Phone numbers are not set")
 
 # Use the service synchronously
-response = sms.send("Hello Message!", ["+254732898412"])
-print(response)
+response = sms.send("Hello Message!", phone_numbers)
 
 # Or use it asynchronously
 def on_finish(error, response):
     if error is not None:
         raise error
-    print(response)
 
-sms.send("Hello Message!", ["+254732898412"], callback=on_finish)
+sms.send("Hello Message!", phone_numbers, callback=on_finish)
